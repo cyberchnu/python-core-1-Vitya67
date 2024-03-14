9b74f6b344dd3701636a3679895a18eb8a148559
Vitya67 <kapitansd35@gmail.com>
2024-03-14 15:42:24 +0200

    task 9

--- src/sum_even_nums_in_range.py
+++ src/sum_even_nums_in_range.py
@@ -1,3 +1,10 @@
 def sum_even_nums_in_range(start, stop):
   # Type your code
-  return
+  if not isinstance(start, int) or not isinstance(stop, int) or start > stop:
+    return None
+
+  total = 0
+  for number in range(start, stop + 1):
+    if number % 2 == 0:
+      total += number
+  return total
