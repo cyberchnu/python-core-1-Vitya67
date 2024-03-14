96877e01945f559ce757fb9347f79da1ccbf6427
Vitya67 <kapitansd35@gmail.com>
2024-03-14 15:41:05 +0200

    task 8

--- src/squares_sum.py
+++ src/squares_sum.py
@@ -1,3 +1,9 @@
 def squares_sum(n):
   # Type your code
-  return
+  if not isinstance(n, int) or n < 1:
+    return None
+
+  total = 0
+  for number in range(1, n + 1):
+    total += number ** 2
+  return total
