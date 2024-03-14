6cb59c05002a509b79a5b652cf9c004ae7ac5b5c
Vitya67 <kapitansd35@gmail.com>
2024-03-14 15:43:39 +0200

    task 10

--- src/mean.py
+++ src/mean.py
@@ -1,3 +1,13 @@
 def mean(number):
   # Type your code
-  return 
+  if not isinstance(number, int) or number < 0:
+    return None
+
+  total = 0
+  digits = 0
+  while number > 0:
+    digit = number % 10
+    total += digit
+    digits += 1
+    number //= 10
+  return total // digits
